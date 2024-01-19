d2794fc171a393fd3fb3d0cebfd666214c6dcdd3
Issei Kuzumaki <14265239+isseikz@users.noreply.github.com>
2024-01-19 13:12:58 +0900

    Fix suggestion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,7 @@ dependencies {
     implementation("androidx.hilt:hilt-work:1.1.0")
     implementation("androidx.datastore:datastore-core:1.0.0")
     implementation("androidx.datastore:datastore-preferences-core:1.0.0")
-    implementation("androidx.preference:preference:1.2.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
