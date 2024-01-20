534a4c937176b6d1b36d66d830049027bb2b9859
Issei Kuzumaki <14265239+isseikz@users.noreply.github.com>
2024-01-20 16:56:58 +0900

    Hide logs for debugging purpose

--- app/src/main/java/com/isseikz/backlogeditor/ApplicationMain.kt
+++ app/src/main/java/com/isseikz/backlogeditor/ApplicationMain.kt
@@ -26,7 +26,9 @@ class ApplicationMain: Application(), Configuration.Provider  {
 
     override fun onCreate() {
         super.onCreate()
-        Timber.plant(Timber.DebugTree())
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
         Timber.d("onCreate")
 
         CoroutineScope(Dispatchers.IO).launch {
