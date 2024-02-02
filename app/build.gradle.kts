5740c3948eb3c7862d5914307ea0d7527a03ae8d
Issei Kuzumaki <14265239+isseikz@users.noreply.github.com>
2024-02-02 13:40:30 +0900

    Bump up version to v1.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.isseikz.backlogeditor"
         minSdk = 31
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
