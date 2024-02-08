664b10af4f7460718c1507979b1e7e346574bcf9
Issei Kuzumaki <14265239+isseikz@users.noreply.github.com>
2024-02-08 22:35:43 +0900

    Bump up version to v1.0.11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.isseikz.backlogeditor"
         minSdk = 31
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.0.10"
+        versionCode = 11
+        versionName = "1.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
