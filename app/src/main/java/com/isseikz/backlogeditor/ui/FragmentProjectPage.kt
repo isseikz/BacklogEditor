ee2509f6e15f4e07d4d3b41403709fd7be9b0e01
Issei Kuzumaki <14265239+isseikz@users.noreply.github.com>
2024-02-08 16:26:23 +0900

    Fix inconsistency of data in ProjectItemsAdapter

--- app/src/main/java/com/isseikz/backlogeditor/ui/FragmentProjectPage.kt
+++ app/src/main/java/com/isseikz/backlogeditor/ui/FragmentProjectPage.kt
@@ -6,16 +6,15 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.isseikz.backlogeditor.R
 import com.isseikz.backlogeditor.data.BacklogItem
-import com.isseikz.backlogeditor.data.ProjectInfo
 import com.isseikz.backlogeditor.source.BacklogRepository
-import com.isseikz.backlogeditor.store.WidgetProjectRepository
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -38,36 +37,29 @@ class FragmentProjectPage(private val projectId: String): Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val project = backlogRepository.getProjectInfo(projectId) ?: run {
-            Timber.w("project is null with projectId $projectId")
-            return
-        }
-
-        view.findViewById<TextView>(R.id.title_project).text = project.projectName
-
+        val singleProjectAdapter = ProjectItemsAdapter()
+        val tvProjectTitle = view.findViewById<TextView>(R.id.title_project)
         view.findViewById<RecyclerView>(R.id.items_list).apply {
             layoutManager = LinearLayoutManager(context)
-            adapter = ProjectItemsAdapter(backlogRepository)
+            adapter = singleProjectAdapter
         }
-    }
-
-    inner class ProjectItemsAdapter(
-        private val backlogRepository: BacklogRepository
-    ) : RecyclerView.Adapter<ProjectItemsAdapter.ProjectItemViewHolder>() {
-        private val scope = CoroutineScope(Dispatchers.IO)
-        val projects: List<BacklogItem>
-            get() = backlogRepository.projectsFlow.value[projectId]?.items ?: listOf()
-
-        init {
-            scope.launch {
-                backlogRepository.projectsFlow.collect {
-                    withContext(Dispatchers.Main) {
-                        notifyDataSetChanged()
-                    }
+        viewLifecycleOwner.lifecycleScope.launch {
+            backlogRepository.projectsFlow.collect { projects ->
+                projects[projectId]
+                    ?.let {
+                        withContext(Dispatchers.Main) {
+                            tvProjectTitle.text = it.projectName
+                            singleProjectAdapter.submitList(it.items)
+                        }
+                    } ?: run {
+                    Timber.w("project is null with projectId $projectId")
                 }
             }
         }
+    }
 
+    class ProjectItemsAdapter :
+        ListAdapter<BacklogItem, ProjectItemsAdapter.ProjectItemViewHolder>(BacklogItemDiffCallback()) {
         inner class ProjectItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
             fun bind(title: String) {
                 itemView.findViewById<TextView>(R.id.project_list_item_title).text = title
@@ -75,16 +67,23 @@ class FragmentProjectPage(private val projectId: String): Fragment() {
         }
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProjectItemViewHolder {
-            val view = LayoutInflater.from(parent.context).inflate(R.layout.project_list_item, parent, false)
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.project_list_item, parent, false)
             return ProjectItemViewHolder(view)
         }
 
-        override fun getItemCount(): Int {
-            return projects.size
+        override fun onBindViewHolder(holder: ProjectItemViewHolder, position: Int) {
+            holder.bind(getItem(position).title)
+        }
+    }
+
+    class BacklogItemDiffCallback : DiffUtil.ItemCallback<BacklogItem>() {
+        override fun areItemsTheSame(oldItem: BacklogItem, newItem: BacklogItem): Boolean {
+            return oldItem.id == newItem.id
         }
 
-        override fun onBindViewHolder(holder: ProjectItemViewHolder, position: Int) {
-            holder.bind(projects[position].title)
+        override fun areContentsTheSame(oldItem: BacklogItem, newItem: BacklogItem): Boolean {
+            return oldItem == newItem
         }
     }
 }
